9bf769088b66530c0cf4101d3472d51e357620df
Codder <122482673+Jhonbrave@users.noreply.github.com>
2023-01-12 18:33:07 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,28 +1,27 @@
-# (c) @RoyalKrrishna
-
+# (c) kryptonian coder
 import os
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 
 
 class Config(object):
-    API_ID = int(os.getenv("API_ID", 12345))
-    API_HASH = os.getenv("API_HASH", "")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-    BOT_SESSION_NAME = os.getenv("BOT_SESSION_NAME", "MdiskSearchRobot")
-    USER_SESSION_STRING = os.getenv("USER_SESSION_STRING", "")
-    CHANNEL_ID = int(os.getenv("CHANNEL_ID", -100))
-    BOT_USERNAME = os.getenv("BOT_USERNAME")
-    BOT_OWNER = int(os.getenv("BOT_OWNER"))
-#    OWNER_USERNAME = os.getenv("OWNER_USERNAME")
-    BACKUP_CHANNEL = os.getenv("BACKUP_CHANNEL")
+    API_ID = int(os.getenv("API_ID", 21971883))
+    API_HASH = os.getenv("API_HASH", "4f76ca506dab1a0b2ddec6db20b4b209")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "5851110616:AAGxEXc76vsovtSdoPjbhy8Q9OPkI9kP-kI")
+    BOT_SESSION_NAME = os.getenv("BOT_SESSION_NAME", "BRAVESTONE")
+    USER_SESSION_STRING = os.getenv("USER_SESSION_STRING", "1BVtsOJ4Bu1PiQNp7auk3VhvX6PmJ_JJnRNeNtnx2s-xG99Mv0z4OPqMAP_Ib2SJVql5c5mvDQApNTNzLPBABGnUjDRD4jHAxbiv-U-RH3U_QZUckhMtkqsVAyxbdVBrb5tk92NWOSwmVHpwy4JteADMf_TfGsOQj6aNQ3IuypP58pMyB9_zhqNn5ajBorkMHpFylasdvPFiNUxnUHP97VNlB7n6EULrjhyEtlRXhrxpoG9HIttxn0ES13RLv_p2nDcMhzG8Wwlu4D6-YSLCyTjm7UhEcNODLxvn99lDhzWWI70ikc75LUfFTx4RzjwW7qwUY-pq2C66XGYsvzLl9ezRpItTB5ek=")
+    CHANNEL_ID = int(os.getenv("CHANNEL_ID", -1001805094850))
+    BOT_USERNAME = os.getenv("BOT_USERNAME", "Bravemoviesprovider_bot")
+    BOT_OWNER = int(os.getenv("BOT_OWNER", 1739321854))
+#    OWNER_USERNAME = os.getenv("OWNER_USERNAME", "Jackkjack")
+    BACKUP_CHANNEL = os.getenv("BACKUP_CHANNEL", "A12111111111")
 #    GROUP_USERNAME = os.getenv("GROUP_USERNAME")
-    START_MSG = os.getenv("START_MSG", '''Hᴇʏ Bᴜᴅᴅʏ! 😃
+    START_MSG = os.getenv("START_MSG", '''Hᴇʏ Boss! 
 
 I'ᴍ A Bᴏᴛ Fᴏʀ Sᴇɴᴅɪɴɢ Fʀᴏᴍ Yᴏᴜʀ Cʜᴀɴɴᴇʟ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ.😚
 
-Yᴏᴜ Cᴀɴ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ.☺️
+Yᴏᴜ Cᴀɴ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ first contact me to get access.☺️
 
 Fᴏʀ Mᴏʀᴇ Iɴꜰᴏ Cʟɪᴄᴋ Oɴ Hᴇʟᴘ ✅''')
     START_PHOTO = os.getenv("START_PHOTO", "https://telegra.ph/file/3073c7543fc3ab93659d9.jpg")
@@ -30,15 +29,15 @@ Fᴏʀ Mᴏʀᴇ Iɴꜰᴏ Cʟɪᴄᴋ Oɴ Hᴇʟᴘ ✅''')
 
 ɪ ᴄᴀɴ ᴘʀᴏᴠɪᴅᴇ ᴛʜᴇʀᴇ ʏᴏᴜʀ ʟɪɴᴋꜱ,
 ꜰᴏʀ ᴍᴏʀᴇ ɪɴꜰᴏ ᴄʟɪᴄᴋ ᴏɴ ʜᴇʟᴘ ✅''')
-    UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", None)
-    DATABASE_URL = os.getenv("DATABASE_URL", "")
-    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", ""))
-    RESULTS_COUNT = int(os.getenv("RESULTS_COUNT", 5))
+    UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", "-1001758473425")
+    DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://Jhonsnow:16jhonsnow16@cluster0.sswioug.mongodb.net/?retryWrites=true&w=majority")
+    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1001654343251"))
+    RESULTS_COUNT = int(os.getenv("RESULTS_COUNT", 8))
     BROADCAST_AS_COPY = os.getenv("BROADCAST_AS_COPY", "True")
-    UPDATES_CHANNEL_USERNAME = os.getenv("UPDATES_CHANNEL_USERNAME", "")
+    UPDATES_CHANNEL_USERNAME = os.getenv("UPDATES_CHANNEL_USERNAME", "ahhahahahssdd")
     FORCE_SUB = os.getenv("FORCE_SUB", "False")
-    AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", 300))
-    MDISK_API = os.getenv("MDISK_API", "12334")
+    AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", 100))
+    MDISK_API = os.getenv("MDISK_API", "xerksgsIlGJBRm3TCNRo")
     VERIFIED_TIME  = int(os.getenv("VERIFIED_TIME", "365"))
     ABOUT_BOT_TEXT = os.getenv("ABOUT_TEXT", '''I ᴏɴʟʏ ꜱʜᴀʀᴇ ᴛʜᴇ ᴘᴏꜱᴛ ꜰʀᴏᴍ ᴘᴇᴏᴘʟᴇ'ꜱ ᴄʜᴀɴɴᴇʟ! ᴡʜᴏ ᴍᴀᴅᴇ ᴍᴇ ᴀᴅᴍɪɴ ɪɴ ᴛʜᴀᴛ ᴄʜᴀɴɴᴇʟ, i ᴅᴏ ɴᴏᴛ ꜱᴛᴏʀᴇ ᴀɴʏ ꜰɪʟᴇꜱ ᴏʀ ᴛᴇxᴛ ɪɴ  ᴍʏ ᴅᴀᴛᴀʙᴀꜱᴇ.✅
 
