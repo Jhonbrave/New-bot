27adb5415f05d55dea210fd2860f7fb328418496
Codder <122482673+Jhonbrave@users.noreply.github.com>
2023-01-28 13:20:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://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@github.com/Jhonbrave/New-bot
+git clone https://github.com/Jhonbrave/New-bot
 cd /MdiskWalaBot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
